967e1d5b64835f1f205d8340ee2d8261ae276c5f
Fabricio Maruta <fabricio.maruta@gmail.com>
2019-06-08 12:16:06 -0300

    Steg using Q matrix of JPEG compress working succesfully

---
+++ JSTEG/trycon.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageTransform
+
+image = Image.open('sample.jpg')
+#generate dict containing arrays
+response_ar = image.quantization
+print("Digite o que deseja esconder:")
+hidden = input()
+a =  []
+for f in hidden:
+    a.append(f)
+response_ar[0][1] = 0
+i=0
+print(response_ar)
+while i < len(response_ar[0]):
+    if i < len(a):
+        response_ar[0][i] = ord(a[i])
+    
+    #response_ar[1][i] = 200
+    i += 1
+
+print(response_ar)
+image.save("out.jpg", qtables=response_ar)
+
