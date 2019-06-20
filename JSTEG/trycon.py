26b33a1efbd8bb662185e1717d65138fb5d48e33
Fabricio Maruta <fabricio.maruta@gmail.com>
2019-06-20 18:03:23 -0300

    Removed prints

--- JSTEG/trycon.py
+++ JSTEG/trycon.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageTransform
-
-image = Image.open('sample.jpg')
+print("Digite o nome do arquivo de imagem em que a esteganografia sera feita:")
+source = input()
+image = Image.open(source)
 #generate dict containing arrays
 response_ar = image.quantization
 print("Digite o que deseja esconder:")
@@ -10,14 +11,11 @@ for f in hidden:
     a.append(f)
 response_ar[0][1] = 0
 i=0
-print(len(response_ar[0]))
-print(len(response_ar[1]))
 while i < len(response_ar[0]):
     response_ar[0][0] = len(a)
     if i < len(a):
         response_ar[0][i+1] = ord(a[i])
     #response_ar[1][i] = 200
     i += 1
-print(response_ar)
 image.save("out.jpg", qtables=response_ar)
-
+print("Imagem gerada no arquivo out.jpg")
--- JSTEG/trydec.py
+++ JSTEG/trydec.py
@@ -1,12 +1,13 @@
 from PIL import Image, ImageTransform
-
-image = Image.open("out.jpg")
+print("Digite a imagem que deseja decifrar:")
+source = input()
+image = Image.open(source)
 response = image.quantization
-print(response)
 message_len = response[0][0]
 message = []
 i = 1
-while i < message_len :
+while i <= message_len :
     message.append(chr(response[0][i]))
     i += 1
+message = (''.join(message))
 print(message)
