eae194dde5299637d88b0aea618beaa0236e01a4
Fabricio Maruta <fabricio.maruta@gmail.com>
2019-06-20 18:37:27 -0300

    Added commentaries

--- JSTEG/trycon.py
+++ JSTEG/trycon.py
@@ -7,6 +7,7 @@ response_ar = image.quantization
 print("Digite o que deseja esconder:")
 hidden = input()
 a =  []
+#Cria vetor contendo mensagem a ser escondida
 for f in hidden:
     a.append(f)
 response_ar[0][1] = 0
@@ -14,8 +15,7 @@ i=0
 while i < len(response_ar[0]):
     response_ar[0][0] = len(a)
     if i < len(a):
-        response_ar[0][i+1] = ord(a[i])
-    #response_ar[1][i] = 200
+        response_ar[0][i+1] = ord(a[i]) 
     i += 1
 image.save("out.jpg", qtables=response_ar)
 print("Imagem gerada no arquivo out.jpg")
--- JSTEG/trydec.py
+++ JSTEG/trydec.py
@@ -1,11 +1,14 @@
 from PIL import Image, ImageTransform
 print("Digite a imagem que deseja decifrar:")
 source = input()
+#Abre a imagem
 image = Image.open(source)
+#Gera a matriz de quantizacao da imagem
 response = image.quantization
 message_len = response[0][0]
 message = []
 i = 1
+#Tenta recuperar a mensagem
 while i <= message_len :
     message.append(chr(response[0][i]))
     i += 1
